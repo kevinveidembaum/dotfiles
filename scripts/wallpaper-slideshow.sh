eac7591526493b5638df591db7a3cbae9a8f8f8e
Kevin Nikolai <nikolai10052005@gmail.com>
2024-11-12 11:06:38 -0300

    fix: wallpaper scrip timer fixed

--- scripts/wallpaper-slideshow.sh
+++ scripts/wallpaper-slideshow.sh
@@ -4,15 +4,14 @@
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 
 # Interval in seconds between wallpaper changes
-INTERVAL=600
+INTERVAL=1800
 
 # Loop forever
 while true; do
   # Find all image files in the wallpaper directory with .jpg or .png extensions
   # and select one randomly
   feh --randomize --bg-scale "$WALLPAPER_DIR"/*.{jpg,png}
-  
+
   # Wait for the specified interval before changing again
   sleep $INTERVAL
 done
-
